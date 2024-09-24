c809c81019509b9cba94ac7a7f1fadf8ea6c0a08
camward <camward@yandex.ru>
2024-09-24 23:53:57 +0400

    backend. использование swagger npm install --save @nestjs/swagger
    http://localhost:3000/api
    http://localhost:3000/api-yaml

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from "@nestjs/swagger";
+
+class HelloWordDto {
+  @ApiProperty()
+  messages: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWordDto
+  })
+  getHello(): HelloWordDto {
+    return { messages: this.appService.getHello() };
   }
 }
