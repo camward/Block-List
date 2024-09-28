a7ca47f4490b6a679816e842bd91623981e27baa
camward <camward@yandex.ru>
2024-09-28 22:18:24 +0400

    backend. авторизация. метод регистрации
    npm i @nestjs/jwt cookie-parser
    npm i -D @types/cookie-parser

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,13 +1,19 @@
-import {Body, Controller, Get, HttpCode, HttpStatus, Post} from '@nestjs/common';
+import {Body, Controller, Get, HttpCode, HttpStatus, Post, Res} from '@nestjs/common';
 import {GetSessionInfoDto, SignInBodyDto, SignUpBodyDto} from "./dto";
 import {ApiCreatedResponse, ApiOkResponse} from "@nestjs/swagger";
+import {AuthService} from "./auth.service";
+import { CookieService } from "./cookie.service";
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private authService: AuthService, private cookieService: CookieService) {}
+
     @Post('sign-up')
     @ApiCreatedResponse()
-    signUp(@Body body: SignUpBodyDto) {
-        return null;
+    async signUp(@Body body: SignUpBodyDto, @Res({ passthrough: true }) res: Response) {
+        const { accessToken } = await this.authService.signUp(body.email, body.password);
+        this.cookieService.setCookie(res, accessToken);
     }
 
     @Post('sign-in')
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from "../users/users.service";
+import { PasswordService } from "./password.service";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private userService: UsersService,
+        private passwordService: PasswordService,
+        private jwtService: JwtService
+    ) {}
+
+    async signUp(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+
+        if (user) {
+            throw new BadRequestException({ type: 'email-exists' });
+        }
+
+        const salt = this.passwordService.getSalt();
+        const hash = this.passwordService.getHash(password, salt);
+
+        const newUser = await this.userService.create(email, hash, salt);
+
+        const accessToken = await this.jwtService.signAsync({ id: newUser.id, email: newUser.email });
+
+        return { accessToken };
+    }
+
+    signIn(email: string, password: string) {}
+}
