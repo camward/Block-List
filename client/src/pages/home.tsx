0c4bcf987d5af9a9017c0e3490d2a8b2857f54b8
camward <camward@yandex.ru>
2024-10-07 22:11:59 +0400

    frontend. установка orval. генерация запросов и DTO
    npm i orval -D
    npm i axios
    npm i -D prettier
    
    npm run api:download
    npm run api:generate

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import localFont from "next/font/local";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../../pages/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../../pages/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
