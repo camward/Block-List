f5336ee8e38d1a01f2a20d9e5a79dac0f5a4fbc1
camward <camward@yandex.ru>
2024-10-13 13:57:29 +0400

    extension. перенос entities из модуля client

---
+++ extension/src/entities/block-list/queries.ts
@@ -0,0 +1,44 @@
+import {
+  blockListControllerAddBlockItem,
+  blockListControllerGetList,
+  blockListControllerRemoveBlockItem,
+} from '@/shared/api/generated';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+const blockListKey = ['block-list'] as unknown[];
+
+export function useBlockListQuery({ q }: { q?: string }) {
+  return useQuery({
+    queryKey: blockListKey.concat([{ q }]),
+    queryFn: () => blockListControllerGetList({ q }),
+    // keepPreviousData: true,
+  });
+}
+
+export function useAddBlockItemMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: blockListControllerAddBlockItem,
+    async onSettled() {
+      await queryClient.invalidateQueries({
+        queryKey: blockListKey,
+        refetchType: 'active',
+      });
+    },
+  });
+}
+
+export function useRemoveBlockItemMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: blockListControllerRemoveBlockItem,
+    async onSettled() {
+      await queryClient.invalidateQueries({
+        queryKey: blockListKey,
+        refetchType: 'active',
+      });
+    },
+  });
+}
