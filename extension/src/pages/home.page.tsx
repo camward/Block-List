bbf2abf9b7077ca78c480ae4ad879bdf45fca992
camward <camward@yandex.ru>
2024-10-14 22:01:17 +0400

    extension. перенос toggle-blocking из модуля client

--- extension/src/pages/home.page.tsx
+++ extension/src/pages/home.page.tsx
@@ -1,3 +1,4 @@
+import { ToggleBlockingButton } from "@/features/toggle-blocking/ui/toggle-blocking-button";
 import { ADMIN_URL } from "@/shared/constants/tabs";
 import { createTab } from "@/shared/lib/browser";
 import { UiButton } from "@/shared/ui/ui-button";
@@ -7,6 +8,7 @@ export function HomePage() {
     return (
         <div className="p-8 flex flex-col gap-3 ">
             <UiLogo />
+            <ToggleBlockingButton />
             <UiButton variant="outlined" onClick={() => createTab(ADMIN_URL)}>
                 Manage extension
             </UiButton>
