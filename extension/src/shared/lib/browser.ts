a5c73291b91571187f337bfd5c9ad9c58094ed65
camward <camward@yandex.ru>
2024-10-15 20:56:22 +0400

    extension. работа с chrome api
    https://developer.chrome.com/docs/extensions/reference/api/declarativeNetRequest?hl=ru
    
    https://developer.chrome.com/docs/extensions/reference/api/alarms?hl=ru

--- extension/src/shared/lib/browser.ts
+++ extension/src/shared/lib/browser.ts
@@ -1,3 +1,34 @@
 export const createTab = (url: string) => {
     chrome.tabs.create({ url });
 };
+
+export type NetRule = chrome.declarativeNetRequest.Rule;
+export const NetRuleActionType = chrome.declarativeNetRequest.RuleActionType;
+export const NetRuleResourceType = chrome.declarativeNetRequest.ResourceType;
+
+export const setNetRules = async (newRules: NetRule[]) => {
+    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+    const oldRuleIds = oldRules.map((rule) => rule.id);
+
+    await chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: oldRuleIds,
+        addRules: newRules,
+    });
+};
+
+export const setBrowserInterval = async (
+    name: string,
+    timeout: number,
+    bg: () => void,
+) => {
+    await chrome.alarms.create(name, {
+        delayInMinutes: timeout / (1000 * 60),
+        periodInMinutes: timeout / (1000 * 60),
+    });
+
+    chrome.alarms.onAlarm.addListener((alarm) => {
+        if (name === alarm.name) {
+            bg();
+        }
+    });
+};
